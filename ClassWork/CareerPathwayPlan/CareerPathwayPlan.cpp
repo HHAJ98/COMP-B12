333d2a8d1422dc52cd9c8430a50510a79ea68291
AJ Ferraro <60527694+HHAJ98@users.noreply.github.com>
2020-02-05 20:12:30 -0800

    Add programming assignment #01

---
+++ ClassWork/CareerPathwayPlan/CareerPathwayPlan.cpp
@@ -0,0 +1,21 @@
+/* 
+Author: Ferraro, AJ
+Assignment: Programming Assignment #01
+Course: COMP B12
+Instructor: Richard Miles
+Date submitted: 2020/01/22
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name:\n\tAJ Ferraro"<<endl;
+    cout << "Major:\n\tComputer Science"<<endl;
+    cout << "Ideal Job:\n\tSoftware deelopment in a large city"<<endl;
+    cout << "Transfer schools:\n\tUCLA\n\tCalPoly"<<endl;
+    cout << "What are your greatest impediments to your goals?\n\tMy long-term planning skills could still use some work."<<endl;
+    cout << "What do you plan to do to achive your goal?\n\tWork hard in my classes, finish my degree, and take advantage of the \n\topportunities I have while still in school."<<endl;
+
+    return 0;
+}
