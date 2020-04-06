574ac78d4f35e713d5384397ecc0b26956734fc2
HHAJ98 <ajferraroprofessional@gmail.com>
2020-04-05 22:29:18 -0700

    Got linked list assignment working

--- ClassWork/LinkedLists/LinkedList.h
+++ ClassWork/LinkedLists/LinkedList.h
@@ -1,42 +1,75 @@
+// A class to represent a node of a linked list
+#include <iostream>
+using namespace std;
+
 struct node {
-    int value;
-    node *next;
+	int value;
+	node *next;
 };
 
-class linkedList{
+class linkedList {
 public:
-    linkedList();
-    void append(int);
-    void deleteFirstInstanceOf(int);
+	linkedList();
+	void append(int);
+	bool deleteFirstInstanceOf(int);
+	void printList();
 private:
-    node head;
-    node dummy;
-    node tail;
-    node *lastNode;
+	node *head;
+	node *tail;
 };
 
-linkedList::linkedList(){
-    tail.next = nullptr;
-    dummy.next = &tail;
-    head.next = &dummy;
-    
-    lastNode = &dummy;
+linkedList::linkedList() {
+	head = nullptr;
+	tail = nullptr;
 }
 
-void linkedList::append(int value){
-    node newNode;
-    newNode.value = value;
-    newNode.next = &tail;
+void linkedList::append(int value) {
+	node *newNode = new node;
+	newNode->value = value;
+	newNode->next = nullptr;
 
-    (*lastNode).next = &newNode;
-
-    lastNode = &newNode;
+	if (head == nullptr){
+		head = newNode;
+		tail = newNode;
+	} else {
+		tail->next = newNode;
+		tail = tail->next;
+	}
 }
 
-void linkedList::deleteFirstInstanceOf(int value){
-	node *pointerToCurrNode = &dummy;
-	while((*pointerToCurrNode).value != value && (*pointerToCurrNode).next){
-    	pointerToCurrNode = (*pointerToCurrNode).next;
-		
-    }
+bool linkedList::deleteFirstInstanceOf (int value) {
+	node *previousNode = head;
+	node *currentNode = head;
+	
+	if (head->value == value) {
+		head = head->next;
+		delete currentNode;
+		return true;
+	}
+
+	while (currentNode->next != nullptr && currentNode->value != value){
+		previousNode = currentNode;
+		currentNode = currentNode->next;
+	}
+
+	if (currentNode->value == value) {
+		previousNode->next = currentNode->next;
+		delete currentNode;
+		return true;
+	}
+	return false;	
+}
+
+void linkedList::printList() {
+	if (head == nullptr) {
+		cout << "Empty List" << endl;
+	} else {
+		node *currentNode = head;
+		cout << "[ ";
+		while (currentNode->next != nullptr) {
+			cout << currentNode->value << ", ";
+			currentNode = currentNode->next;
+		}
+		cout << currentNode->value << " ]" << endl;
+	}
 }
--- ClassWork/LinkedLists/main.cpp
+++ ClassWork/LinkedLists/main.cpp
@@ -1,8 +1,53 @@
-#include <iostream>
+/*
+Author: Ferraro, AJ
+Assignment: Programming Assignment #05
+Course: COMP B12
+Instructor: Richard Miles
+Date submitted: 2020/##/##
+*/
+#include <string>
 #include "LinkedList.h"
 
-using namespace std;
-int main(){
-    linkedList list;
-    return 0;
+
+void greetUser();
+
+int main() {
+	linkedList list;
+	bool hasQuit = false;
+	string command;
+	int value;
+	greetUser();
+	do {
+		cout << ">> ";
+		cin >> command;
+		if (command != "app" && command != "del") {
+			cout << "quitting..." << endl;
+			hasQuit = true;
+		} else if (command == "app") {
+			cin >> value;
+			list.append(value);
+			cout << "List: ";
+			list.printList();
+		} else if (command == "del") {
+			cin >> value;
+			if (!list.deleteFirstInstanceOf(value))
+				cout << "couldn't find a node with that value, please try another value." << endl;
+			cout << "List: ";
+			list.printList();
+		}
+	} while (!hasQuit);
+	return 0;
+}
+
+void greetUser() {
+	string greeting = "Hello! To append a value to the list, type \"app\" followed by the value you wish\n"
+	"to append to the list. To delete the first occurance of a given value in the\n"
+	"list, type \"del\" followed by the value you wish to delete. Type \"quit\" to exit.\n"
+	"eg:\n"
+	"app 1\n"
+	"del 24\n"
+	"quit\n"
+	"-------------------------------------------------------------------------------";
+
+	cout << greeting << endl;
 }
